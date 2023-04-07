b9344360ebd2b8bf75812bfc68c38be09297cf88
Hoang21002 <dinhhoang026@gmail.com>
2023-04-08 03:26:38 +0700

    Xong Slider

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,9 @@ let navbar = document.querySelector('.header .nav-item');
 document.querySelector('.menu-bnt').onclick = () => {
     navbar.classList.toggle('active');
 };
+
+flatpickr("#myID", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+});
